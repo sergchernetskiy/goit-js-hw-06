1eb883d26604db23aeceaecb58677eefeb02117c
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-06 20:56:27 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listRef = document.querySelector("#ingredients");
+/* console.log(listRef); */
+
+const makeIngredientsCard = (ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  return itemEl;
+};
+
+listRef.append(...ingredients.map(makeIngredientsCard));
