f50611205911abc89457462780fd750dacdcd684
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-10 13:57:58 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const refs = {
+  body: document.querySelector("body"),
+  color: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onColorChange(event) {
+  const randomColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = randomColor;
+  refs.color.textContent = randomColor;
+}
+
+refs.btn.addEventListener("click", onColorChange);
