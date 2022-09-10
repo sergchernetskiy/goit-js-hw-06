ab7c1feb13cfb22dc1d96ab69aeea5f3f48722c4
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-10 12:01:32 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormRef = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  /*   event.currentTarget.elements.email.value
+  event.currentTarget.elements.password.value; */
+
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть, будь ласка, всі поля!");
+  }
+
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    console.log("value", value);
+    console.log("name", name);
+  });
+
+  event.currentTarget.reset();
+}
+
+loginFormRef.addEventListener("submit", onFormSubmit);
