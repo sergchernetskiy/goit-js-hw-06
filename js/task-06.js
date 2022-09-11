8b226905726966d050b92831a22fbb329fdaff69
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-11 14:44:29 +0300

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -4,15 +4,27 @@ console.log(inputRef);
 function onInputFocus(event) {
   const requiredLength = Number(event.target.dataset.length);
 
-  event.target.classList.add("invalid");
-  event.target.classList.remove("valid");
+  onAddClass("invalid");
+  onRemoveClass("valid");
+  /* event.target.classList.add("invalid");
+  event.target.classList.remove("valid"); */
   /* console.log(requiredLength);
   console.log(event.target.value.length); */
 
   if (requiredLength === event.target.value.length) {
-    event.target.classList.remove("invalid");
-    event.target.classList.add("valid");
+    onAddClass("valid");
+    onRemoveClass("invalid");
+    /* event.target.classList.remove("invalid");
+    event.target.classList.add("valid"); */
   }
+}
+
+function onAddClass(add) {
+  event.target.classList.add(add);
+}
+
+function onRemoveClass(remove) {
+  event.target.classList.remove(remove);
 }
 
 inputRef.addEventListener("blur", onInputFocus);
